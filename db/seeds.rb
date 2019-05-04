61dcb7cbfc2b7ace6e45cc1a1c917ddc708adbeb
Victor Giraldes <victorpgiraldes@gmail.com>
2019-05-04 18:36:14 -0400

    creating DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
 
 
-premises = [{name: "teste", address: "Location 1"},
-            {name: "teste2", address: "Location 2"}
+premises = [
+  {name: "teste", address: "Location 1"},
+  {name: "teste2", address: "Location 2"}
 ]
 
 premises.each do |p|
   Premise.create(p)
+end
+
+stations = [
+  {serial: "#{rand(1000000)}", name: "station 1", premise_id: 1},
+  {serial: "#{rand(1000000)}", name: "station 2", premise_id: 1},
+  {serial: "#{rand(1000000)}", name: "station 3", premise_id: 2},
+  {serial: "#{rand(1000000)}", name: "station 4", premise_id: 2}
+]
+
+stations.each do |s|
+  Station.create(s)
 end
